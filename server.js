da721daee8005bb0eadfd03de145d1a8cef00beb
linnykerxd9 <linnykerxd9@gmail.com>
2021-09-19 19:22:54 -0300

    Add jwt in project

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const env = require('dotenv')
 const express = require('express')
 const { Sequelize } = require('sequelize');
+const jwt = require('jsonwebtoken');
 
-env.config()
-const app = express()
-
+env.config();
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = process.env.PORT || 3003;
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
+const JWT_PASSWORD =  process.env.JWT_PASSWORD;
+
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
@@ -30,6 +34,12 @@ app.get('/', (req, res) => {
   return res.send('API de cadastro de usuários!')
 })
 
+app.post('/login',(req, res) => {
+  const user = req.body.user;
+  const token = jwt.sign(user,JWT_PASSWORD);
+
+  res.send(token);
+})
 
 app.listen(port, () => { 
   console.log('API iniciada na porta ', port)
