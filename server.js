8910ef9fbbba9d30721a6e579d8da3836c95dcb8
linnykerxd9 <linnykerxd9@gmail.com>
2021-09-19 17:54:50 -0300

    add database connection

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const env = require('dotenv')
 const express = require('express')
-
+const { Sequelize } = require('sequelize');
 
 env.config()
 const app = express()
 
 
-const port = process.env.PORT || 3003
+const port = process.env.PORT || 3003;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
 
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'postgres'
+});
+
+try {
+   sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.get('/', (req, res) => {
   return res.send('API de cadastro de usuários!')
